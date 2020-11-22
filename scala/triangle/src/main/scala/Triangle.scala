e34d8ee88d7e221d9dd4120bf3f62efc5ac1b93b
Jakob Ledig <Jakob.Ledig@evodion.de>
2020-11-22 15:40:01 +0100

    Triangles solution

--- scala/triangle/src/main/scala/Triangle.scala
+++ scala/triangle/src/main/scala/Triangle.scala
@@ -1,3 +1,14 @@
 case class Triangle(x: Double, y: Double, z: Double) {
+  val sides = Set(x, y, z)
 
+  private val triangleInequality: Boolean = {
+    val everySide = List(x, y, z)
+    everySide.map(side => side <= (everySide.sum - side)).min
+  }
+
+  private val nonZero: Boolean = !sides.contains(0)
+
+  val equilateral: Boolean = sides.size <= 1 && nonZero
+  val isosceles: Boolean = Set(x, y, z).size <= 2 && nonZero && triangleInequality
+  val scalene: Boolean = Set(x, y, z).size >= 3 && nonZero && triangleInequality
 }
