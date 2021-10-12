166024f88be543359751310e8c134c7090b4f5b2
Jakob Ledig <j.ledig@digitec.de>
2021-10-12 09:39:11 +0200

    got rid of inner loop with default parameters

--- scala/prime-factors/src/main/scala/PrimeFactors.scala
+++ scala/prime-factors/src/main/scala/PrimeFactors.scala
@@ -1,15 +1,11 @@
 import scala.annotation.tailrec
 
 object PrimeFactors {
-  def factors(n: Long): List[Long] = {
-    @tailrec
-    def factorsRec(n: Long, div: Long, acc: List[Long]): List[Long] = {
-      if (div > n) acc else {
-        if (n % div == 0) factorsRec(n / div, div, acc :+ div)
-        else factorsRec(n, div + 1, acc)
-      }
+  @tailrec
+  def factors(n: Long, div: Long = 2, acc: List[Long] = List()): List[Long] = {
+    if (div > n) acc else {
+      if (n % div == 0) factors(n / div, div, acc :+ div)
+      else factors(n, div + 1, acc)
     }
-
-    factorsRec(n, 2, List())
   }
 }
