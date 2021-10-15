67a890f25798bf0457cf04b8c27440d03554b7f4
Jakob Ledig <j.ledig@digitec.de>
2021-10-15 16:11:18 +0200

    All tests fine

--- scala/grade-school/src/main/scala/GradeSchool.scala
+++ scala/grade-school/src/main/scala/GradeSchool.scala
@@ -1,12 +1,23 @@
 class School {
   type DB = Map[Int, Seq[String]]
+  private var database: DB = Map()
 
-  def add(name: String, g: Int) = ???
+  def add(name: String, g: Int): Unit = {
+    this.database = this.database.get(g) match {
+      case Some(names) => this.database.updated(g, names :+ name)
+      case None => this.database + (g -> Seq(name))
+    }
+  }
 
-  def db: DB = ???
+  def db: DB = database
 
-  def grade(g: Int): Seq[String] = ???
+  def grade(g: Int): Seq[String] = database.getOrElse(g, Seq.empty)
 
-  def sorted: DB = ???
+  def sorted: DB = database
+    .keys
+    .toList
+    .sorted
+    .map(grade => (grade, database(grade).sorted)) // safe because we already know the grade-index is taken from the map
+    .toMap
 }
 
