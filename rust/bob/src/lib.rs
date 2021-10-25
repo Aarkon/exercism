9713e0b05a140419eeab7914fb60886b7e99ead0
Jakob Ledig <j.ledig@digitec.de>
2021-10-25 09:41:19 +0200

    Rust: Bob refactoring

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,20 +1,23 @@
 pub fn reply(raw_message: &str) -> &str {
-    let message = raw_message.trim();
-    let question = message.ends_with("?");
-    let contains_letters = message.chars().any(char::is_alphabetic);
-    let yelled = message.to_uppercase() == message && contains_letters;
+    let message: &str = raw_message.trim();
+    let question: bool = message.ends_with("?");
+    let contains_letters: bool = message.chars().any(char::is_alphabetic);
+    let yelled: bool = message.to_uppercase() == message && contains_letters;
 
-    if message.len() == 0 {
-        return "Fine. Be that way!";
+    const FINE: &str = "Fine. Be that way!";
+    const CALM_DOWN: &str = "Calm down, I know what I'm doing!";
+    const SURE: &str = "Sure.";
+    const CHILL_OUT: &str = "Whoa, chill out!";
+    const WHATEVER: &str = "Whatever.";
+
+    if message.is_empty() {
+        return FINE;
     }
-    if yelled && question {
-        return "Calm down, I know what I'm doing!";
+
+    match (yelled, question) {
+        (true, true) => CALM_DOWN,
+        (_, true) => SURE,
+        (true, _) => CHILL_OUT,
+        _ => WHATEVER
     }
-    if question {
-        return "Sure.";
-    }
-    if yelled {
-        return "Whoa, chill out!";
-    }
-    "Whatever."
 }
