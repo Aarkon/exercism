033ebf4104775fcf5495ddab22c462a73af81678
Jakob Ledig <j.ledig@digitec.de>
2021-10-25 09:30:54 +0200

    Scala: Bob solution

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,3 +1,14 @@
 object Bob {
-  def response(statement: String): String = ???
+  def response(raw_statement: String): String = {
+    val statement = raw_statement.trim()
+    val isQuestion = statement.endsWith("?")
+    val containsLetters = statement.exists(_.isLetter)
+    val isYelled = statement.toUpperCase == statement && containsLetters
+
+    if (statement.isBlank) "Fine. Be that way!"
+    else if (isYelled && isQuestion) "Calm down, I know what I'm doing!"
+    else if (isYelled) "Whoa, chill out!"
+    else if (isQuestion) "Sure."
+    else "Whatever."
+  }
 }
