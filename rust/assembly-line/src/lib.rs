873ac01bf1a1225f26b34d6cc5f5dd3c98a4d152
Jakob Ledig <j.ledig@digitec.de>
2021-10-26 09:58:09 +0200

    Rust: assembly-line solution

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -2,10 +2,17 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
+const FACTOR: f64 = 221.0;
+
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    match speed {
+        1..=4 => FACTOR * speed as f64 * 1.0,
+        5..=8 => FACTOR * speed as f64 * 0.9,
+        9 | 10 => FACTOR * speed as f64 * 0.77,
+        _ => 0.0
+    }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed) / 60.0) as u32
 }
